83b33a46747c24d3005c4667ac53046e319cc030
langley <langley228@gmail.com>
2023-08-18 01:32:27 +0800

    ad

--- run.bat
+++ run.bat
@@ -2,4 +2,5 @@ cd src
 docker-compose -f docker-compose.jmeter.env.yml -p test up -d mongo
 docker-compose -f docker-compose.jmeter.yml -p test up
 
-docker cp jenkins:/opt/data/result .
+docker cp jmeter:/opt/data/result .\
+dir
