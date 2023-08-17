44172f2fbe37fc7584eb24e404d8b0125877d856
langley <langley228@gmail.com>
2023-08-18 06:47:19 +0800

    4

--- src/Dockerfiles/Jmeter/test-plan/mongo/test.sh
+++ src/Dockerfiles/Jmeter/test-plan/mongo/test.sh
@@ -1,5 +1,1 @@
-rm /opt/data/result/mongo.log /
-rm /opt/data/result/mongo.jtl /
-rm -rf /opt/data/result/reports/mongo /
-mkdir -p /opt/data/result/reports/mongo /
 jmeter -Dlog_level.jmeter=DEBUG -n -t /opt/data/test-plan/mongo/mongo.jmx -l /opt/data/result/mongo.jtl -j /opt/data/result/mongo.log -e -o /opt/data/result/reports/mongo/
