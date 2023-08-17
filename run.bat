44195056a29531de1c098c6c3b8f5947c968d6c3
langley <langley228@gmail.com>
2023-08-18 01:58:11 +0800

    ADD

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 cd src
-docker-compose -f docker-compose.jmeter.env.yml -p test up -d mongo
-docker-compose -f docker-compose.jmeter.yml -p test up
+docker-compose -f docker-compose.jmeter.env.yml -p test up -d --build mongo
+docker-compose -f docker-compose.jmeter.yml -p test up --build
 
 docker cp test-jmeter-1:/opt/data/result .\
 dir
