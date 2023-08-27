bd19dff81c72289c09c875f13b84b248a21026e7
Langley Chuang <Langley_Chuang@migocorp.com>
2023-08-27 17:07:27 +0800

    2

--- script/run-jmeter.sh
+++ script/run-jmeter.sh
@@ -3,6 +3,7 @@ export COMPOSE_NAME=jmeter-test-$(date +%s)
     # 建立 Jmeter 測試環境 (docker-compose.yml 雷同, 但 port 不轉接給 host )   
         # image 用完即丟, 一律加上 --build
         # 目前只有 mongo 其他暫時不需要
+cd ./../src
 docker compose -f docker-compose.jmeter.env.yml -p $COMPOSE_NAME up -d --build mongo
     # 不能背景執行, 否則報表出不來
 docker compose -f docker-compose.jmeter.yml -p $COMPOSE_NAME up --build
